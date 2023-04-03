4643cbc8077502bd141a86bfcb35f73ac6ce81d0
DiogoRodrigues007 <diogogamerpudim@gmail.com>
2023-04-02 22:57:27 -0300

    Adicionando o zero

--- script.js
+++ script.js
@@ -20,12 +20,15 @@ function countdown() {
     const finalMinutos = Math.floor(segTotal / 60) % 60
     const finalSegundos = Math.floor(segTotal) % 60
 
-    dias.innerHTML = `${finalDias}D`
-    horas.innerHTML = `${finalHoras}H`
-    minutos.innerHTML = `${finalMinutos}M`
-    segundos.innerHTML = `${finalSegundos}S`
+    dias.innerHTML = finalDias + 'D'
+    horas.innerHTML = formatoTempo(finalHoras) + 'H'
+    minutos.innerHTML = formatoTempo(finalMinutos) + 'M'
+    segundos.innerHTML = formatoTempo(finalSegundos) + 'S'
 }
 
+function formatoTempo(tempo){
+    return tempo < 10? `0${tempo}` : tempo;
+}
 
 countdown()
 setInterval(countdown, 1000)
